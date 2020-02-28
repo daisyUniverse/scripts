ed16a9b8a28a8b6fabeb281b5ec1b876e1466fc2
Evshaddock <evshaddock@gmail.com>
2020-02-28 03:33:03 -0600

    Fixed some problems with the blur script, made it move to /usr/bin/

--- blur/blur.py
+++ blur/blur.py
@@ -6,13 +6,13 @@ from pathlib import Path
 pipe = sys.stdin.read()
 home = str(Path.home())
 if pipe:
-    window_class = pipe.replace('"','')
+    window_class = pipe.replace('"','').strip()
     opacity = hex(int(sys.argv[(len(sys.argv) - 1)]))
 else:
     window_class = str(sys.argv[(len(sys.argv) - 2)])
     opacity = hex(int(sys.argv[(len(sys.argv) - 1)]))
 
-args = '( if \n( contains ( window_class ) "' + window_class + '" )\n( begin\n( spawn_async (str "xprop -id " (window_xid) " -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 ") ) \n( spawn_async (str "xprop -id " (window_xid) " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY ' + opacity + 'fffffe") ) \n) \n)'
+args = '( if \n( contains ( window_class ) "' + window_class + '" )\n( begin\n( spawn_async (str "xprop -id " (window_xid) " -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 ") ) \n( spawn_async (str "xprop -id " (window_xid) " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY ' + opacity + 'ffffff") ) \n) \n)'
 
 f = open(home + '/.devilspie/' + window_class + '.ds','w+')
 f.write(args)
--- blur/readme.md
+++ blur/readme.md
@@ -2,11 +2,10 @@
 
 make it shiny, please
 
-1. `./blur [opacity (0-255)]`
-
-2. Click on the application you want to blur
-
-3. Restart devilspie
+1. `./blur install` (installs `blur` and `blur.py` to /usr/bin/)
+2. `blur [opacity (0-255)]`
+3. Click on the application you want to blur
+4. Restart devilspie
 
 Requires: 
 
