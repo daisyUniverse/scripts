eb1f17c12aa6190108f15cd103c68f9ae8674770
Evshaddock <evshaddock@gmail.com>
2020-11-16 20:47:41 -0600

    Updated the cloth server management scripts

--- minecraft-server-management/cloth/clothBot.py
+++ minecraft-server-management/cloth/clothBot.py
@@ -4,6 +4,7 @@ import requests
 import os
 import sys
 import math
+import subprocess
 
 prefix = '>>'
 
@@ -16,10 +17,14 @@ commands = { # Command names, change these to change the command trigger
     "wipe"     : "wipe",
     "announce" : "announce",
     "config"   : "config",
-    "help"     : "help"
+    "help"     : "help",
+    "command"  : "command",
+    "killworld": "killworld",
+    "version"  : "version",
+    "repo"     : "repo"
 }
 
-allowedUsers = ["128423195366785024","719545123096231956"]
+allowedUsers = ["128423195366785024","719545123096231956","537334651631435826","766496300140593152"]
 
 async def log(text, user, loglevel):
     if loglevel == 'LOW':
@@ -29,10 +34,44 @@ async def log(text, user, loglevel):
     elif loglevel == 'HIGH':
         print('\033[31m[HIGH] ' + user + ": "+  text + "\033[0m")
 
+async def autoUpdater():
+    while True:
+        channel = client.get_channel(776542321981390859)
+        URL = ("https://api.github.com/repos/Luminoso-256/Cloth-Server/releases/latest")
+        try:
+            r = requests.get(url = URL)
+            if r.ok:
+                data = r.json()
+                latest = (data['name'])
+
+                with open("PCV", "r") as f: 
+                    current = f.read()
+
+                if current != data['tag_name']:
+                    with open("PCV", "w") as f: 
+                        f.write(data['tag_name'])
+
+                    await log("Updated server to " + data['tag_name'] + " from " + current, "Auto", "HIGH")
+                    await channel.edit(topic=(latest))
+                    await channel.send("A new update of cloth was detected: **" + data['tag_name'] + "**, updating from the current version **" + current + "**")
+                    subprocess.Popen(["bash","clothServer","-u"])   
+            else:
+                await log("Tried to check for an update for the server but didn't get a 200 OK response from Github \n" + str(r.json()), "[AUTO]", "HIGH")
+        except Exception as e:
+            print(traceback.format_exc())
+            if hasattr(e, 'message'):
+                await channel.send("```python\nError on line " + str(sys.exc_info()[-1].tb_lineno) + "\n" + str(e.message) + "```")
+            else:
+                await channel.send("```python\nError on line " + str(sys.exc_info()[-1].tb_lineno) + "\n" + str(e) + "```")
+
+        await asyncio.sleep(300)
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
       print ('\033[1mWelcome to the Cloth Server Manager by \33[31mRobin Universe\033[0m')
       print ('Bot core loaded as user:', self.user)
+      await autoUpdater()
 
     async def on_message(self, message):
         msgCaps = message.content
@@ -73,13 +112,34 @@ class MyClient(discord.Client):
 
                     if msg.startswith(commands['wipe']):
                         await log(commands['wipe'] + " command used", str(message.author), "HIGH")
-                        await message.channel.send("Wiping the userdata of " + msg[len(commands['wipe'])+1:] + " and restarting the server... ")
-                        os.system('bash clothServer -w ' + msg[len(commands['wipe'])+1:])
+                        await message.channel.send("Wiping the userdata of " + msgCaps.split(" ")[1])
+                        os.system('bash clothServer -w ' + msgCaps.split(" ")[1])
 
                     if msg.startswith(commands['announce']):
                         await log(commands['announce'] + " command used", str(message.author), "LOW")
-                        await message.channel.send("Updating the Cloth server...")
-                        os.system('bash clothServer -a "' + msg[len(commands['announce'])+1:] + '"')
+                        await message.channel.send("Sent to the server")
+                        os.system('bash clothServer -a "' + msgCaps[len(commands['announce'])+3:] + '"')
+
+                    if msg.startswith(commands['killworld']):
+                        await log(commands['killworld'] + " command used", str(message.author), "HIGH")
+                        await message.channel.send("Deleting world and restarting the server...")
+                        os.system('bash clothServer --DESTROY')
+
+                    if msg.startswith(commands['version']):
+                        await log(commands['version'] + " command used", str(message.author), "LOW")
+                        with open("PCV", "r") as f: 
+                            current = f.read()
+                        await message.channel.send("The current running version of Cloth is " + current)
+
+                    if msg.startswith(commands['repo']):
+                        await log(commands['repo'] + " command used", str(message.author), "LOW")
+                        await message.channel.send("https://github.com/Luminoso-256/Cloth-Server")
+
+                    if msg.startswith(commands['command']):
+                        await log("server command " + msgCaps[len(commands['announce'])+2:] + " used", str(message.author), "MED")
+                        await message.channel.send("Executing command `" + msgCaps[len(commands['announce'])+2:] + "` on server")
+                        output = subprocess.check_output('bash clothServer -c "' + msgCaps[len(commands['announce'])+2:] + '"', shell=True)
+                        await message.channel.send("**SERVER:** `" + output.decode('ascii', errors='ignore') + "`")
 
                     if msg.startswith(commands['config']):
                         file = msg.split(" ")[1]
@@ -114,9 +174,13 @@ class MyClient(discord.Client):
                             "`"+prefix + "restart` Restarts the cloth server" + "\n" +
                             "`"+prefix + "update` Shuts down the cloth server, pulls latest release of cloth, and starts it again" + "\n" +
                             "`"+prefix + "stop` Stops the cloth server" + "\n" +
-                            "`"+prefix + "wipe <username>` wipes the playerdata and restarts server" + "\n" +
+                            "`"+prefix + "wipe <username>` wipes the playerdata" + "\n" +
                             "`"+prefix + "announce <text>` will do a say command (eventualy)" + "\n" +
+			                "`"+prefix + "command <command>` will do a command" + "\n" +
+                            "`"+prefix + "killworld` Deletes the world. This is not reversable." + "\n" +
                             "`"+prefix + "config <property.file> <property=newvalue>` updates a property in a file" + "\n" +
+                            "`"+prefix + "version` Shows the version of Cloth Server the server is currently running" + "\n" +
+                            "`"+prefix + "repo` links the github repo" + "\n" +
                             "`"+prefix + "help` take a wild fuckin guess buddy" + "\n")
 
                 except Exception as e: # returns error meesages to discord in a python codeblock for debugging
